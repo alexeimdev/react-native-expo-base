0a85ea78dcf154595fce25255279c9a8a1cbec7a
alexeimdev <alexeim.dev@gmail.com>
2019-03-10 00:46:29 +0200

    added navigation

--- App.js
+++ App.js
@@ -1,32 +1,34 @@
 import React from 'react';
 import {
     StyleSheet,
-    View,
     SafeAreaView,
     StatusBar,
     KeyboardAvoidingView,
-    Platform,
 } from 'react-native';
 
 import Colors from './constants/Colors';
 import Shadow from './constants/Shadow';
-import MainScreen from './components/screens/MainScreen';
+import AppContainer from './navigation/AppContainer';
 
 const App = (props) => {
     return (
         <SafeAreaView style={styles.safeAreaView}>
             {/* <StatusBar style={styles.statusBar} /> */}
             {/* <View style={{ height: 100, backgroundColor: Colors.purple, ...Shadow.small }} /> */}
-            <KeyboardAvoidingView behavior="padding">
-                <MainScreen />
+            <KeyboardAvoidingView style={styles.keyboardAvoidingView} behavior="padding">
+                <AppContainer />
             </KeyboardAvoidingView>
         </SafeAreaView>
+
     );
 }
 
 const styles = StyleSheet.create({
     safeAreaView: {
-        backgroundColor: Colors.transparent,
+        flex: 1,
+    },
+    keyboardAvoidingView: {
+        flex: 1,
     },
     statusBar: {
         height: StatusBar.currentHeight,
---
+++ navigation/AppNavigator.js
@@ -0,0 +1,9 @@
+import { createStackNavigator } from 'react-navigation';
+
+import MainScreen from '../components/screens/MainScreen';
+
+const AppNavigator = createStackNavigator({
+    MainScreen,
+})
+
+export default AppNavigator;
